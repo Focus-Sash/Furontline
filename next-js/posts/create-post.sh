c7659adc47e4ff3c5719e567eed3a1f26ad0363b
Focus-Sash <tenma.quo.ast@gmail.com>
2022-07-21 00:35:23 +0900

    Edit posts

---
+++ next-js/posts/create-post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+title=$1
+filename=$2
+category=$3
+path="${category}/${filename}.md"
+date=`date '+%F'`
+touch ${path}
+echo "---" >> ${path}
+echo "title: '${title}'" >> ${path}
+echo "summary: ' '" >> ${path}
+echo "date: '${date}'" >> ${path}
+echo "tags:" >> ${path}
+echo "category: '${category}'" >> ${path}
+echo "---" >> ${path}
+echo "" >> ${path}
